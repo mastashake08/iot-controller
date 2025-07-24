663e8c4dfb18de81511aa276a06c861ef3e80197
Hacker God <jyrone.parker@gmail.com>
2025-07-24 00:12:49 -0400

    feat: add Docker configuration and update PHPUnit for testing environment

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -7,8 +8,16 @@ Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
+Route::get('dashboard', function (Request $request) {
+   $user = $request->user();
+
+    // For Stripe Cashier
+    $balance = $user->balance(); // Or $user->balance if you store it, or use Stripe API
+
+    return Inertia::render('Dashboard', [
+        'balance' => $balance,
+        // ...other props
+    ]);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/settings.php';
